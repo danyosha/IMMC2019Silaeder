f2e32bb279297b1bf9543337af12fbffa7d8acd5
danyosha <danben0429@gmail.com>
2019-04-08 15:53:47 +0300

    Ya rodilcya

--- Planet.py
+++ Planet.py
@@ -1,4 +1,9 @@
 class Earth:
     def next_year_update(self):
-        for i in range(len(self.people)):
-            self.people[i] += (self.people[i]) / 80
+        self.now_people = len(self.people) * n
+        self.now_people += (self.now_people) / 80
+        self.new_groups = (self.now_people / n) - len(self.people)
+        for i in range(self.new_groups):
+            x = random(-1000, 1000)
+            y = random(-1000, 1000)
+            self.people.append(x, y)
