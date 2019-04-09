4ef05bc4d6c3241dd0358b349109b95aa4a54259
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2019-04-09 12:14:06 +0300

    Delete Planet.py

--- Planet.py
+++
@@ -1,9 +0,0 @@
-class Earth:
-    def next_year_update(self):
-        self.now_people = len(self.people) * n
-        self.now_people += (self.now_people) / 80
-        self.new_groups = (self.now_people / n) - len(self.people)
-        for i in range(self.new_groups):
-            x = random(-1000, 1000)
-            y = random(-1000, 1000)
-            self.people.append([x, y])
