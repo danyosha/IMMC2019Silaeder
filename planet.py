e3c85fe5a37e871f67b3c28afc82764764c31707
danyosha <danben0429@gmail.com>
2019-04-09 14:57:08 +0300

    replaced bagis in rogdenie

--- planet.py
+++ planet.py
@@ -1,49 +1,59 @@
 import pygame
 import random
+
 class Istota(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
 class Water(Istota):
     pass
+
 class Land(Istota):
     pass
+
 class People(Istota):
     pass
+
 class Planet(object):
     def __init__(self):
         self.water = []
         self.land = []
         self.people = []
-        
+
     def add(self, new_object):
-        if(type(new_object) == water)
-            self.water.append(self.water)
-        if(type(new_object) == land)
-            self.land.append(self.land)
-        if(type(new_object) == people)
-            self.people.append(self.people)
-    def update():
+        if(type(new_object) == Water):
+            self.water.append(new_object)
+        if(type(new_object) == Land):
+            self.land.append(new_object)
+        if(type(new_object) == People):
+            self.people.append(new_object)
+
+    def update(self):
         self.new_generation = set()
         for i in range(len(self.people)):
-            self.new_generation.insert(self.people[i])
-            if ([self.people[i].x + 1, self.people[i].y not in set(self.water) and [self.people[i].x + 1, self.people[i].y not in set(self.people)):
+            print(type(self.people))
+            self.new_generation.add(self.people[i])
+            if ([self.people[i].x + 1, self.people[i].y] not in set(self.water) and [self.people[i].x + 1, self.people[i].y] not in set(self.people)):
                 chance = random.randint(1, 1000)
                 if (chance < 13):
-                    self.new_generation.insert([self.people[i].x + 1, self.people[i].y])
+                    self.new_generation.add(People(self.people[i].x + 1, self.people[i].y]))
             if ([self.people[i].x - 1, self.people[i].y] not in set(self.water) and [self.people[i].x - 1, self.people[i].y] not in set(self.people)):
                 chance = random.randint(1, 1000)
                 if (chance < 13):
-                    self.new_generation.insert([self.people[i].x - 1, self.people[i].y])
+                    self.new_generation.add(People(self.people[i].x - 1, self.people[i].y))
             if ([self.people[i].x, self.people[i].y + 1] not in set(self.water) and [self.people[i].x, self.people[i].y + 1] not in set(self.people)):
                 chance = random.randint(1, 1000)
                 if (chance < 13):
-                    self.new_generation.insert([self.people[i].x, self.people[i].y + 1])
+                    self.new_generation.add(People(self.people[i].x, self.people[i].y + 1))
             if ([self.people[i].x, self.people[i].y - 1] not in set(self.water) and [self.people[i].x, self.people[i].y - 1] not in set(self.people)):
                 chance = random.randint(1, 1000)
                 if (chance < 13):
-                    self.new_generation.insert([self.people[i].x, self.people[i].y - 1])
+                    self.new_generation.add(People(self.people[i].x, self.people[i].y - 1))
         self.people = list(self.new_generation)
 
+
+
         
-                    
+
+                   
